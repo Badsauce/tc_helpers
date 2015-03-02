7f469bf2e5b8bbe3695ca42adf05f91bbb476165
badsauce <ninjazoby@gmail.com>
2015-03-02 11:40:49 -0500

    time done

--- helpers/methods_practice_helpers.rb
+++ helpers/methods_practice_helpers.rb
@@ -1,4 +1,5 @@
 module MethodsPracticeHelpers
+  require 'active_support/core_ext/time'
 
   def image_tag(source, alternate_text)
     return "<img class='img-responsive img-thumbnail' src='#{source}' alt='#{alternate_text}' />"
@@ -12,9 +13,7 @@ module MethodsPracticeHelpers
   end
 
   def current_date_and_time
-    # Give the current date and time, in the format: "February 6, 2015 at 4:25pm"
-    # Time.now.strftime is a great method chain you can use for this purpose.
-    # See how to use it at: http://apidock.com/ruby/Time/strftime
+    return Time.now.in_time_zone("America/Detroit").strftime("%B %-e, %Y at %-l:%M%P")
   end
 
   # ------------------------------------------
